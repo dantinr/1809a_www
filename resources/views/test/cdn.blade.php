72f2347a8a3700ce89eec7e7a1f0e4ae9e8e1331
dante <dantinr@gmail.com>
2019-09-24 14:40:48 +0800

    debug

--- resources/views/test/cdn.blade.php
+++ resources/views/test/cdn.blade.php
@@ -5,12 +5,12 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href={{env('CDN_HOST')}}"/css/index.css">
+    <link rel="stylesheet" href="{{env('CDN_HOST')}}/css/index.css">
     <title>CDN测试</title>
 </head>
 <body>
     <h1>CDNCDN</h1>
 
-    <script src={{env('CDN_HOST')}}"/js/index.js"></script>
+    <script src="{{env('CDN_HOST')}}/js/index.js"></script>
 </body>
 </html>
